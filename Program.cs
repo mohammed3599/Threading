908daa3c784a02dd767662398dae22dc4e263511
mohammed3599 <kataloni1999@gmail.com>
2023-07-23 11:42:04 +0400

    use background thread

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Thread thread = new Thread(new ThreadStart(doSomeWork));
+            thread.IsBackground = true; //To use Background thread
             thread.Start();
 
             for(int i = 0; i < 10; i++)
@@ -12,7 +13,7 @@
                 Console.WriteLine("Main Thread: {0}", i);
                 Thread.Sleep(10);
             }
-            thread.Join();
+            //thread.Join(); //To prevent stopping the doSomework thread from debugging
             Console.WriteLine("Main block");
         }
         public static void doSomeWork() 
