338294c47aae668ed43e14154b57316ab3a8034c
mohammed3599 <kataloni1999@gmail.com>
2023-07-23 13:15:56 +0400

    await and async

--- Program.cs
+++ Program.cs
@@ -1,20 +1,40 @@
-﻿namespace Threading
+﻿using System.ComponentModel;
+
+namespace Threading
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int someValue = 10;
+        static bool isLocked = false;
+        static async Task Main(string[] args)
         {
-            Thread thread = new Thread(new ThreadStart(doSomeWork));
-            thread.IsBackground = true; //To use Background thread
-            thread.Start();
+            //Thread thread = new Thread(new ThreadStart(doSomeWork));
+            //thread.IsBackground = true; //To use Background thread
+            //thread.Start();
 
-            for(int i = 0; i < 10; i++)
-            {
-                Console.WriteLine("Main Thread: {0}", i);
-                Thread.Sleep(10);
-            }
-            //thread.Join(); //To prevent stopping the doSomework thread from debugging
-            Console.WriteLine("Main block");
+            //for(int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Main Thread: {0}", i);
+            //    Thread.Sleep(10);
+            //}
+            ////thread.Join(); //To prevent stopping the doSomework thread from debugging
+            //Console.WriteLine("Main block");
+
+            //Thread thread1 = new Thread(increamentVariable);
+            //Thread thread2 = new Thread(increamentVariable);
+
+            //thread1.Start();
+            //thread2.Start();
+
+            //thread1.Join();
+            //thread2.Join();
+
+            //Console.WriteLine(someValue);
+
+            int result = await add(2, 3);
+            Console.WriteLine(result);
+            Console.WriteLine("Hello");
+
         }
         public static void doSomeWork() 
         {
@@ -23,6 +43,24 @@
                 Console.WriteLine("Worker Thread: {0}", i);
                 Thread.Sleep(100);
             }
+        }
+        public static void increamentVariable()
+        {
+            if (!isLocked)
+            {
+                isLocked = true;
+                for (int i = 0;i < 1000; i++)
+                {
+                    someValue++;
+                }
+                isLocked = false;
+            }
+        }
+
+        public static  async Task<int> add(int a, int b)
+        {
+            //await Task.Delay(5000);
+            return a + b;
         }
     }
 }
