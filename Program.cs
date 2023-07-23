cfacdb3f395f2185e178b79fd52590b0d4edefbc
mohammed3599 <kataloni1999@gmail.com>
2023-07-23 10:55:40 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(doSomeWork));
+            thread.Start();
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main Thread: {0}", i);
+                Thread.Sleep(10);
+            }
+            thread.Join();
+            Console.WriteLine("Main block");
+        }
+        public static void doSomeWork() 
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Worker Thread: {0}", i);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
